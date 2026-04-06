5abb0aa5d3bec08ac7b59cad22f5755def686927
Naoour <148296892+Naoour@users.noreply.github.com>
2026-04-06 17:07:35 +0700

    gold

---
+++ mdp_gold/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:mdp_gold/screens/price_list_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // Menjalankan aplikasi Flutter dengan widget MainApp
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PriceListScreen(),
+    );
+  }
+}
